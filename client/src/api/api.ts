130d678310e044d47096619eb3f256e72c39b60c
Chirag Varshney <87241050+chirag0002@users.noreply.github.com>
2024-04-02 19:48:19 +0530

    prod

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = 'http://localhost:8080'
+// const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://heliverse-49va.onrender.com'
 
 export const API = axios.create({
     baseURL: BASE_URL
